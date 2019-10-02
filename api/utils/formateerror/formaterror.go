f1f0d2c2e8af708efdefbdf8a8163b6dd08b87d3
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-10-02 13:03:53 +0700

    Create custom error handling

---
+++ api/utils/formateerror/formaterror.go
@@ -0,0 +1,28 @@
+package formateerror
+
+import (
+	"errors"
+	"strings"
+)
+
+/**
+	To format some error messages in a more readable manner, we need to a create a package to help us achieve that.
+ */
+func FormatError(err string) error {
+	if strings.Contains(err, "nickname") {
+		return errors.New("Nickname Already Taken")
+	}
+
+	if strings.Contains(err, "email") {
+		return errors.New("Email Already Taken")
+	}
+
+	if strings.Contains(err, "title") {
+		return errors.New("Title Already Taken")
+	}
+
+	if strings.Contains(err, "hashedPassword") {
+		return errors.New("Incorect Password")
+	}
+	return errors.New("Incorrect Details")
+}
