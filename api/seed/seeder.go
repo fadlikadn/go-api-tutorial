9c47f5774b74f375ea1f22d882df1f8d195c6846
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-10-04 13:40:06 +0700

    Implement seeder class

---
+++ api/seed/seeder.go
@@ -0,0 +1,65 @@
+package seed
+
+import (
+	"github.com/fadlikadn/go-api-tutorial/api/models"
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+var users = []models.User {
+	models.User{
+		Nickname:	"Steven Victor",
+		Email:		"steven@gmail.com",
+		Password: 	"password",
+	},
+	models.User{
+		Nickname: "Martin Luther",
+		Email:    "luther@gmail.com",
+		Password: "password",
+	},
+	models.User{
+		Nickname: "Fadlika Dita Nurjanto",
+		Email:    "fadlikadn@gmail.com",
+		Password: "password",
+	},
+}
+
+var posts = []models.Post {
+	models.Post{
+		Title: 		"Title 1",
+		Content:	"Hello World 1",
+	},
+	models.Post{
+		Title: 		"Title 2",
+		Content:	"Hello World 2",
+	},
+}
+
+func Load(db *gorm.DB) {
+	err := db.Debug().DropTableIfExists(&models.Post{}, &models.User{}).Error
+	if err != nil {
+		log.Fatalf("cannot drop table: %v", err)
+	}
+	err = db.Debug().AutoMigrate(&models.User{}, &models.Post{}).Error
+	if err != nil {
+		log.Fatalf("canot migrate table: %v", err)
+	}
+
+	err = db.Debug().Model(&models.Post{}).AddForeignKey("author_1", "users(id)", "cascade", "cascade").Error
+	if err != nil {
+		log.Fatalf("attaching foreign key error: %v", err)
+	}
+
+	for i, _ := range users {
+		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed users table: %v", err)
+		}
+		posts[i].AuthorID = users[i].ID
+
+		err = db.Debug().Model(&models.Post{}).Create(&posts[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed posts table: %v", err)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -6,9 +6,14 @@ require (
 	github.com/badoux/checkmail v0.0.0-20181210160741-9661bd69e9ad
 	github.com/denisenkom/go-mssqldb v0.0.0-20191001013358-cfbb681360f0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/gorilla/mux v1.7.3
 	github.com/jinzhu/gorm v1.9.11
 	github.com/lib/pq v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc
+	golang.org/x/net v0.0.0-20191003171128-d98b1b443823 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20191003212358-c178f38b412c // indirect
+	golang.org/x/tools v0.0.0-20191004055002-72853e10c5a3 // indirect
 	google.golang.org/appengine v1.6.4 // indirect
 )
