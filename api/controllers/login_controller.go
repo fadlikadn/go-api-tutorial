c7705932415bf5e2ad0239c54fe5cf9896f663ee
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-10-04 13:05:12 +0700

    Implement Login Controller

---
+++ api/controllers/login_controller.go
@@ -0,0 +1,56 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/fadlikadn/go-api-tutorial/api/auth"
+	"github.com/fadlikadn/go-api-tutorial/api/models"
+	"github.com/fadlikadn/go-api-tutorial/api/responses"
+	"github.com/fadlikadn/go-api-tutorial/api/utils/formateerror"
+	"golang.org/x/crypto/bcrypt"
+	"io/ioutil"
+	"net/http"
+)
+
+func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user.Prepare()
+	err = user.Validate("login")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	token, err := server.SignIn(user.Email, user.Password)
+	if err != nil {
+		formattedError := formateerror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusUnprocessableEntity, formattedError)
+		return
+	}
+	responses.JSON(w, http.StatusOK, token)
+}
+
+func (server *Server) SignIn(email, password string) (string, error) {
+	var err error
+	user := models.User{}
+
+	err = server.DB.Debug().Model(models.User{}).Where("email = ?", email).Take(&user).Error
+	if err != nil {
+		return "", err
+	}
+
+	err = models.VerifyPassword(user.Password, password)
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return "", err
+	}
+	return auth.CreateToken(user.ID)
+}
